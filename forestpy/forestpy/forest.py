79fa2f680560532a804eb8f888c639c6e696772f
Rawser Spicer <rwspicer@alaska.edu>
2020-02-06 11:48:44 -0900

    updated njobs

--- forestpy/forestpy/forest.py
+++ forestpy/forestpy/forest.py
@@ -219,11 +219,11 @@ def setup(feature_grid, label_grid, ss_percent=.5):
 
     return ss_features, ss_labels, ss_idx
     
-def brute_force_task(ss_features, ss_labels, name):
+def brute_force_task(ss_features, ss_labels, name, n_jobs=4):
     current_parameters = RFParams(name)
 
     model = create_model(ss_features, ss_labels, current_parameters,
-                verbose=2, n_jobs=4)
+                verbose=2, n_jobs=n_jobs)
 
     # model.fit(ss_features.T, ss_labels)
 
@@ -346,7 +346,7 @@ def setup_brute_force(feature_file, label_file):
 
 
 
-def run_brute_force(computer, progress_file, ss_data_sets):
+def run_brute_force(computer, progress_file, ss_data_sets, n_jobs=4):
     update = None
     while True:
         print ('\n\n')
@@ -364,7 +364,7 @@ def run_brute_force(computer, progress_file, ss_data_sets):
         model = brute_force_task(
             ss_data_sets[tpd]['features'], 
             ss_data_sets[tpd]['labels'], 
-            _next
+            _next, n_jobs
         )
         total = datetime.now() - start
 
