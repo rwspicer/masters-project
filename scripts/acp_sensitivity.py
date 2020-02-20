0da538632602bbd7b7c55ce27727e9974cfe24cc
Rawser Spicer <rwspicer@alaska.edu>
2020-02-19 22:54:14 -0900

    fix

--- scripts/acp_sensitivity.py
+++ scripts/acp_sensitivity.py
@@ -4,12 +4,12 @@ sys.path.insert(0, os.path.abspath('../forestpy/forestpy'))
 from datetime import datetime
 import os
 import joblib
-
+import forest
 
 import numpy as np
 from multigrids import TemporalMultiGrid, TemporalGrid
 
-data_path = '/home/rwspicer/data/V1'
+data_path = '/Volumes/toshi-stati/data/V1/'
 
 feature_root = os.path.join(data_path, 'master-project/training/ACP/v2/')
 
@@ -27,7 +27,7 @@ items = [
             feature_root,'with-labels/multigrid/with-labels.yml' 
         ),
         'label_file': label_file ,
-        'precent':75
+        'percent':75
 
     },
     {
@@ -36,7 +36,7 @@ items = [
             feature_root,'with-labels/multigrid/with-labels.yml' 
         ),
         'label_file': label_file ,
-        'precent':75
+        'percent':75
 
     },
     {
@@ -45,7 +45,7 @@ items = [
             feature_root,'with-random/multigrid/with-random.yml' 
         ),
         'label_file': label_file ,
-        'precent':75
+        'percent':75
 
     },
     {
@@ -54,7 +54,7 @@ items = [
             feature_root,'no-geolocation/multigrid/no-geolocation.yml' 
         ),
         'label_file': label_file ,
-        'precent':75
+        'percent':75
 
     },
     {
@@ -63,7 +63,7 @@ items = [
             feature_root,'without-elevation/multigrid/without-elevation.yml' 
         ),
         'label_file': label_file ,
-        'precent':75
+        'percent':75
 
     },
 ]
@@ -77,26 +77,37 @@ save_path = '../'
 
 def go():
 
+    loaded_data = {}
     grades = {}
+    #this is always the same
+    sam = items[0]
+    f_gird = TemporalMultiGrid(sam['features_file'])
+    l_grid = TemporalGrid(sam['label_file'])
     for sam in items:
-        hyperparameters = RFParams(sam['name'])
-        f_gird = TemporalMultiGrid(sam['features_file'])
-        l_grid = TemporalGrid(sam['label_file'])
+        print(sam['name'])
+        hyperparameters = forest.RFParams(sam['name'])
+        
 
 
-
-        features, labels, index = setup(f_gird,l_grid, sam['percent'])
+        # if sam['percent'] in loaded_data:
+        #     pass
+        # else:
+        #     print('loading data')
+        print ('loading')
+        features, labels, index = forest.setup(f_gird,l_grid, sam['percent'])
+            #  = 
         start = datetime.now()
+        print ("running_model")
         model = create_model(
             features, 
             labels, 
             hyperparameters, 
             2, # verbosity
-            10 # n_jobs
+            4 # n_jobs
         )
         total_train = datetime.now() - start
         
-        test_features, labels_true = format_data(f_grid, l_grid)
+        test_features, labels_true = forest.format_data(f_grid, l_grid)
         start = datetime.now()
         labels_predicted = model.predict(test_features.T)
         total_predict = datetime.now() - start
@@ -122,5 +133,6 @@ def go():
         save_name = sa_name.split('.')[0] +'_'+ sam['name'].split('.')[0]+'.joblib'
         grades[save_name] = score
         joblib.dump(model, os.path.join(save_path,save_name))
+        print (score)
 
     return grades
