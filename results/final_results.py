9af2c820c276205560d8b56c984ced6e0c010fec
Rawser Spicer <rwspicer@alaska.edu>
2020-04-15 09:20:43 -0800

    updated paper

---
+++ results/final_results.py
@@ -0,0 +1,27 @@
+
+import datetime 
+
+
+final_SP_results = {'train_time': datetime.timedelta(seconds=515, microseconds=408587), 
+'predict_time': datetime.timedelta(seconds=6, microseconds=216531), 
+'features': 'SP-rf-training-set.yml', 
+'mean_error': -0.002202139046493215, 
+'median_error': 0.00552018760044426, 
+'var_error': 0.4333478312355796, 
+'mean_abs_error': 0.31071599086348445, 
+'median_abs_error': 0.17885631838890426, 
+'var_abs_error': 0.3368082536736827,
+ 'r2': 0.9983439342739824}
+
+
+ final_ACP_results = {
+     'train_time': datetime.timedelta(seconds=6376, microseconds=305569),
+      'predict_time': datetime.timedelta(seconds=45, microseconds=463398),
+       'features': 'ACP-training-base.yml', 
+       'mean_error': -0.003439011467619034, 
+       'median_error': 0.013471402346390382, 
+       'var_error': 2.1934920278345107, 
+       'mean_abs_error': 0.9662507502523258, 
+       'median_abs_error': 0.6242368385196553, 
+       'var_abs_error': 1.2598633422712058, 
+       'r2': 0.9885382197841925}
